3889b20fdf689d3852c505fa547dc1720faf6cb4
Tommyluo <tluo12321@yahoo.com>
2015-04-21 13:34:34 -0700

    initial

---
+++ client/index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+angular.module('bank-account', ['firebase'])
+
+.run(['$rootScope', function($rootScope) {
+  $rootScope.balance = 10000;
+  $rootScope.name = 'Bob Smith';
+  $rootScope.penalty = -50;
+}])
+.controller('master', ['$rootScope', '$scope', function($rootScope, $scope) {
+  $scope.deposits = [];
+  $scope.withdraws = [];
+  $scope.fees = [];
+
+  $scope.deposit = function() {
+    var depo = $rootScope.balance += $scope.amount;
+    $scope.deposits.push({amount: $scope.amount, date: new Date()});
+  };
+  $scope.withdraw = function() {
+    var fee = 0;
+    if ($rootScope.balance >= 0 && ($rootScope.balance - $scope.amount < 0)) {
+      fee = 50;
+      $scope.fees.push({amount: 50, date: new Date()});
+    }
+    $rootScope.balance -= $scope.amount + fee;
+    $scope.withdraws.push({amount: $scope.amount, date: new Date()});
+  };
+}]);
